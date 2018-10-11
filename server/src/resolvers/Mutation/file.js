7f69914b0a4c7cc8d68eed9f5ea22b36969b2a62
nenu-git <sebastien.belmon@gmail.com>
2018-10-11 17:17:00 +0200

    WIP update et Delete file

--- server/src/resolvers/Mutation/file.js
+++ server/src/resolvers/Mutation/file.js
@@ -1,6 +1,9 @@
+const prisma = require('prisma-binding');
+
 const { processUpload } = require('../../localImages');
 
 const file = {
+  // UPLOAD A FILE
   async singleUpload(parent, { filename, file }, ctx, info) {
     return ctx.db.mutation.createFile(
       {
@@ -11,6 +14,27 @@ const file = {
       },
       info
     );
+  },
+
+  // TODO !
+  //? Idée : On trouve le fichier sur le serve avec 'fs', et on le supprime
+  //? puis on supprime également le node graphql
+  //* créer une fonction processDeleteFile
+  // A MODIFIER
+  deleteFile: prisma.forwardTo('db'),
+
+  // TODO !
+  //? Idée : On trouve le fichier sur le serve avec 'fs', et on le renomme
+  //? puis on renomme également le node graphql
+  //* créer une fonction processUpdateFile
+  // UPDATE A FILE NAME
+  async updateFile(parent, { where, filename }, ctx, info) {
+    return ctx.db.mutation.updateFile({
+      where,
+      data: {
+        filename,
+      }
+    })
   },
 };
 
